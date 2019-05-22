973f9d1c8582aee3bd132e77d758ab37e84ee41a
hikaruya <hikaruy00805@gmail.com>
2019-05-23 06:50:23 +0900

    整理

---
+++ database.py
@@ -0,0 +1,32 @@
+import os, sys, logging, time, configparser
+from pymongo import MongoClient,  DESCENDING
+import pandas as pd
+
+# Const of database name
+DICTIONARY_DB = "dictionaries"
+PL_COLLECTION_NAME = "polarity"
+
+# db接続情報は、`config.ini`に外だししておく
+# --config.ini sample--------
+# [mongo]
+# id=**
+# password=**
+def get_db(db_name):
+    config = configparser.ConfigParser()
+    config.read( './config.ini')
+    client = MongoClient('localhost')
+    client['admin'].authenticate(config.get('mongo', 'id'), config.get('mongo', 'password'))
+    db = client[db_name]
+    return db
+
+# mongoDBのスキーマ
+# ------------
+# データベース名：dictionarries
+# |- コレクション名：polarity
+#    |- ドキュメント フォーマット： headword, score, ...
+# （ドキュメント例： {headword="悪い", score=-1}, ...） 
+def load_polarity_dic(collection_name):
+    db = get_db(DICTIONARY_DB)
+    cursor = db[collection_name].find()
+    df = pd.DataFrame.from_dict(list(cursor)).astype(object)
+    return df
