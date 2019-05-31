08aa009b0d506ccefea989d5010336b0a34d7006
hikaruya <hikaruy00805@gmail.com>
2019-05-31 14:29:02 +0900

    戻した

--- main.py
+++ main.py
@@ -3,14 +3,13 @@
 import json
 import os
 import sys
-import config
-import oseti
+
 #import logging
 #logging.getLogger().setLevel( logging.DEBUG )
 
-# from google.cloud import language
-# from google.cloud.language import enums
-# from google.cloud.language import types
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
 
 import flask
 
@@ -27,18 +26,18 @@ from linebot.models import (
 app = flask.Flask(__name__)
 
 #CHANNEL_ACCESS_TOKEN
-line_bot_api = LineBotApi('ACCESS_TOKEN') #自分のLINEでのACCESS TOKENを入れる
+line_bot_api = LineBotApi('取得したLINE Message API の <アクセストークン(ロングターム）を設定>')
 #CANNEL_SECRET
-handler = WebhookHandler('CHANNEL_SECRET') #自分のLINEでのCHNNEL SECRETを入れる
+handler = WebhookHandler('<取得したLINE Message API のCANNEL_SECRET　を設定>')
 
 # Instantiates a client
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'JSON_LINE_FILE'
-# client = language.LanguageServiceClient()
+client = language.LanguageServiceClient()
+
 
 #返信メッセージを設定
-msglst ={100:"上機嫌！全然問題ない!!!",
-        90:"上機嫌！問題ない!!",
-        80:"機嫌良し！問題ない!",
+msglst ={100:"全然問題ない!!!",
+        90:"全然問題ない!!",
+        80:"全然問題ない!",
         70:"問題ない!!!",
         60:"問題ない!!",
         50:"問題ない!",
@@ -46,7 +45,7 @@ msglst ={100:"上機嫌！全然問題ない!!!",
         30:"たぶん大丈夫",
         20:"まだ大丈夫",
         10:"もう少し大丈夫",
-        0:"普通",
+        0:"ギリギリ大丈夫",
         -10:"様子見しますか",
         -20:"もう少し様子見しますか",
         -30:"あと少し様子見しますか",
@@ -55,8 +54,8 @@ msglst ={100:"上機嫌！全然問題ない!!!",
         -60:"ちょっとやばいかも。。",
         -70:"少しフォロー必要かも",
         -80:"フォロー必要",
-        -90:"まずは謝ろう",
-        -100:"土下座しよう"
+        -90:"早くフォロー！",
+        -100:"まずは謝る"
     }
 
 @app.route("/")
@@ -83,6 +82,9 @@ def callback():
 
 @handler.add(MessageEvent, message = TextMessage)
 def handle_message(event):
+
+
+
     text = event.message.text
 
     document = types.Document(
@@ -90,9 +92,7 @@ def handle_message(event):
     type=enums.Document.Type.PLAIN_TEXT)
 
     # Detects the sentiment of the text
-    analyzer = oseti.Analyzer()
-    sentiment = analyzer.analyze(document)[0]
-    # sentiment = client.analyze_sentiment(document=document).document_sentiment
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
 
     #score = round(sentiment.score, 1)
     score = int(sentiment.score * 100)
